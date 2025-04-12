53021afec69ecded39cd7ad6a5a87d4ab9fbb1bc
nodokashibasaki3 <nodokaorganized@gmail.com>
2025-04-12 17:04:46 -0400

    Refactor firebase config to use .env variables

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAd0K3KqxitWnfn-QZKTOl7a3ISm99rbug",
-  authDomain: "auxa-85642.firebaseapp.com",
-  projectId: "auxa-85642",
-  storageBucket: "auxa-85642.firebasestorage.app",
-  messagingSenderId: "139611903139",
-  appId: "1:139611903139:web:18b64accc962b118a95b16",
-  measurementId: "G-ZLJKNJSHX4"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
